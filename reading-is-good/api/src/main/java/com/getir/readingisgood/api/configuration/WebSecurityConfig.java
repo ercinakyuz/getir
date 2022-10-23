69339dbfcb9a8aa71a20f3d336d36dc22fa46bbf
Erçin <b.ercin.akyuz@gmail.com>
2022-10-23 05:03:02 +0300

    Java version updated to 18

--- reading-is-good/api/src/main/java/com/getir/readingisgood/api/configuration/WebSecurityConfig.java
+++ reading-is-good/api/src/main/java/com/getir/readingisgood/api/configuration/WebSecurityConfig.java
@@ -2,20 +2,20 @@ package com.getir.readingisgood.api.configuration;
 
 import com.getir.readingisgood.api.filter.JwtTokenFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+public class WebSecurityConfig {
 
     private static final String[] NO_AUTHORIZATION_URLS = new String[]{
             "/lock/**",
@@ -27,9 +27,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
             "/merchant/registration/**",
     };
     private final JwtTokenFilter jwtTokenFilter;
-
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
         http.csrf()
                 .disable()
                 .authorizeRequests()
@@ -40,7 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
         http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 }
